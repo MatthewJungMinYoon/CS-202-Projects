bc832a0939940a240b487b612b39f658fd398aa3
Matthew Yoon <52327338+MatthewJungMinYoon@users.noreply.github.com>
2019-12-03 18:03:35 -0500

    Create proj2.cpp

---
+++ proj2/proj2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "Foundable.h"
+#include "Wizard.h"
+#include "Game.h"
+
+using namespace std;
+
+int main () {
+  srand(time(NULL));
+  Game();
+  return 0;
+}
